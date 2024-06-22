63c234938f00bc9bd6063934338ed94f5a7f193c
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2024-06-22 19:52:28 +0000

    [Fix] book_commentコントローラのcreateアクションの記述の修正

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,7 +2,7 @@ class BookCommentsController < ApplicationController
   def create
     book = Book.find(params[:book_id])
     comment = BookComment.new(book_comment_params)
-    coment.user_id = current_user.id
+    comment.user_id = current_user.id
     comment.book_id = book.id
     comment.save
     redirect_to book_path(book)
