d74e2985adf76f30b35e15fc60d87963e21ba588
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2024-06-22 21:33:00 +0000

    [Add] favoritesコントローラ・ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   
   resources :books, only: [:show, :index, :create, :edit, :update, :destroy] do
     resources :book_comments, only: [:create, :destroy]
+    resource :favorite, only: [:create, :destroy]
   end
 end
