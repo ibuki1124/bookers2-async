bad8bbb63ed7ed5606cd40101739f115a05df6dd
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2024-06-23 11:50:28 +0000

    [Update] ルーティングのパス名・ビューのパス指定を変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root "homes#top"
   get 'home/about' => "homes#about", as: "about"
-  
+
   devise_for :users
   resources :users, only: [:show, :index, :edit, :update]
-  
+
   resources :books, only: [:show, :index, :create, :edit, :update, :destroy] do
     resources :book_comments, only: [:create, :destroy]
-    resource :favorite, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
 end
