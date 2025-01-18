1982a652a3637f94962140a35287e7b2db01b093
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2025-01-18 03:25:55 +0000

    [Add] ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   get 'home/about' => "homes#about", as: "about"
 
   devise_for :users
-  resources :users, only: [:show, :index, :edit, :update]
+  resources :users, only: [:show, :index, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get "followers" => "relationships#followers", as: "followers"
+    get "following" => "relationships#following", as: "following"
+  end
 
   resources :books, only: [:show, :index, :create, :edit, :update, :destroy] do
     resources :book_comments, only: [:create, :destroy]
