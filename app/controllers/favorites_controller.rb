c6851dd0f5d1ba0248818c2863f198d64499a878
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2025-01-25 04:05:58 +0000

    [Update] いいねのコントローラの変更・jsファイルの追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,17 @@
 class FavoritesController < ApplicationController
   def create
     book = Book.find(params[:book_id])
-    favorite = Favorite.new(book_id: book.id)
-    favorite.user_id = current_user.id
-    favorite.save
-    redirect_back fallback_location: root_path
+    @favorite = Favorite.new(book_id: book.id)
+    @favorite.user_id = current_user.id
+    @favorite.save
+    render "replace_favorite"
   end
 
   def destroy
     book = Book.find(params[:book_id])
-    favorite = current_user.favorites
-    favorite = favorite.find_by(book_id: book.id)
-    favorite.destroy
-    redirect_back fallback_location: root_path
+    @favorite = current_user.favorites
+    @favorite = @favorite.find_by(book_id: book.id)
+    @favorite.destroy
+    render "replace_favorite"
   end
 end
